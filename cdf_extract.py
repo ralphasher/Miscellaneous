9d2e07294a0b743a5aa0a17633c3aba025b9040e
Ralph Asher <r_j_asher@hotmail.com>
2021-03-07 17:10:21 -0600

    Update cdf_extract.py

--- cdf_extract.py
+++ cdf_extract.py
@@ -1,6 +1,3 @@
-#import Penn State CDF files and save as CSV
-#leans heavily on https://towardsdatascience.com/cdfs-using-spacepy-to-read-and-write-your-data-conveniently-9490f0bfee4a
-
 #install CDF library from NASA website to local machine
 #https://cdf.gsfc.nasa.gov/html/sw_and_docs.html
 #ensure you get 32bit or 64bit CDF library to match 32bit/64bit python
@@ -15,7 +12,7 @@ import matplotlib.pyplot as plt
 import datetime
 
 #define directory with CDF files
-cdf_dir = 'C:/Users/W530/Documents/USMC/MTVR_CBM_CDF_20210301/'
+cdf_dir = 'C:/my_dir/'
 cdf_files = os.listdir(cdf_dir)
 outlist = list()
 
